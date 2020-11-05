46f30135ac66b0a7391aabb779621c9343b94983
ameedasmah <67121588+ameedasmah@users.noreply.github.com>
2020-11-05 14:18:06 +0200

    connectin

--- server.js
+++ server.js
@@ -1,2 +1,28 @@
 
+const express = require("express");
+let app = express();
+// const {form1,form2,form3}=require("./public/db/index")
+
+app.use(express.static(__dirname +'/public'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.post(,(req,res)=>{
+//     res.status(201).jason(req.body)
+// })
+
+// const data = {}
+// app.get('/app',(req,res)=>{
+//     res.sendFile(__dirname+'/./compiled/public/app.js')
+// })
+
+// app.post('/checkout',(req,res)=>{
+//     console.log(req.body)
+//     res.send(data)
+// })
+app.listen(3000,()=>{
+    console.log('server is working')
+})
+
+
 
